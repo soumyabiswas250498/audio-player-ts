a6e06b4947c1064e94811b5f311ca528210bcace
soumyabiswas250498 <soumyabiswas250498@gmail.com>
2024-09-03 22:35:46 +0530

    grid memoized

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Image from 'next/image';
-import { GridProps } from '../Types';
+import { GridProps, ListCardProps } from '../Types';
+
+
+
+function GridCard(props: ListCardProps) {
+    const { setMusicData, file } = props;
+
+    const imgUrl = useMemo(() => {
+        return file.thumbnailFile ? URL.createObjectURL(file.thumbnailFile) : '/music_img.jpg'
+    }, [file.thumbnailFile])
+
+    return (
+        <div
+
+            className="p-2 transition duration-200 border border-gray-600 rounded-lg cursor-pointer bg-gray-800/10 hover:bg-gray-800"
+            onClick={() => {
+                setMusicData({
+                    imgSrc: file.thumbnailFile ? URL.createObjectURL(file.thumbnailFile) : '/music_img.jpg',
+                    audioSrc: URL.createObjectURL(file.audioFile),
+                    title: file.songTitle,
+                    fileName: file.audioFile.name,
+                });
+            }}
+        >
+            <div className="relative w-full h-32 mb-2">
+                <Image
+                    src={imgUrl}
+                    alt={`${file.songTitle} thumbnail`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-lg"
+                />
+            </div>
+            <h2 className="text-lg font-bold text-gray-300 truncate">{file.songTitle}</h2>
+            <p className="text-sm text-gray-400 truncate">{file.audioFile.name}</p>
+        </div>
+    )
+
+}
 
 
 const Grid: React.FC<GridProps> = ({ fileArr, setMusicData }) => {
@@ -11,30 +49,7 @@ const Grid: React.FC<GridProps> = ({ fileArr, setMusicData }) => {
             ) : (
                 <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-6">
                     {fileArr.map((file, index) => (
-                        <div
-                            key={index}
-                            className="p-2 transition duration-200 border border-gray-600 rounded-lg cursor-pointer bg-gray-800/10 hover:bg-gray-800"
-                            onClick={() => {
-                                setMusicData({
-                                    imgSrc: file.thumbnailFile ? URL.createObjectURL(file.thumbnailFile) : '/music_img.jpg',
-                                    audioSrc: URL.createObjectURL(file.audioFile),
-                                    title: file.songTitle,
-                                    fileName: file.audioFile.name,
-                                });
-                            }}
-                        >
-                            <div className="relative w-full h-32 mb-2">
-                                <Image
-                                    src={file.thumbnailFile ? URL.createObjectURL(file.thumbnailFile) : '/music_img.jpg'}
-                                    alt={`${file.songTitle} thumbnail`}
-                                    layout="fill"
-                                    objectFit="cover"
-                                    className="rounded-lg"
-                                />
-                            </div>
-                            <h2 className="text-lg font-bold text-gray-300 truncate">{file.songTitle}</h2>
-                            <p className="text-sm text-gray-400 truncate">{file.audioFile.name}</p>
-                        </div>
+                        <GridCard file={file} key={index} setMusicData={setMusicData} />
                     ))}
                 </div>
             )}
